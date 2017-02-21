256f57d299179be73514479c107aa96e92fe5a6d
priyaishere <krishnapriyatce@gmail.com>
2017-02-21 14:05:59 +0530

    Update contin.c

--- pkg/C50/src/contin.c
+++ pkg/C50/src/contin.c
@@ -37,9 +37,9 @@
 #include "transform.h"
 #include "redefine.h"
 #include "math.h"
-#include alpha -1.25
-#include q 1/(1-alpha)
-#define	PartInfo(n) (pow(n,alpha))/GEnv.Cases
+#define alpha -1.25
+#define q 1/(1-alpha)
+#define	PartInfo(n) pow(n,alpha)/GEnv.Cases
 
 
 /*************************************************************************/
